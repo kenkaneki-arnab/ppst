f86227ee85b8e6944a38a29c30a014ebfc097502
Arnab Maity <kenk38976@gmail.com>
2026-02-04 00:23:03 +0530

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+int main() { 
+int a, b; 
+cout << "Enter two numbers: "; 
+cin >> a >> b; 
+if (a > b) { 
+cout << a << " is maximum" << endl; 
+} else if (b > a) { 
+cout << b << " is maximum" << endl; 
+} else { 
+cout << "Both numbers are equal" << endl; 
+} 
+return 0; 
+} 
